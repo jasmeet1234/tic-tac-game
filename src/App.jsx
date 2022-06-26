9679409b1691178557f6870448ca1f05882b2840
Jasmeet Singh <jasighotra123@gmail.com>
2022-06-26 13:25:45 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default () => (
+  <React.Fragment>
+    <h1>Welcome to React Vite Micro App!</h1>
+    <p>Hard to get more minimal than this React app.</p>
+    <p>Hello fhgjkl</p>
+  </React.Fragment>
+);
